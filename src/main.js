f8706c8e8c2af6ccc78cdb2d034aed6fde373396
yueming-Chen <aa1235561@gmail.com>
2017-10-26 00:05:09 +0800

    1.設計作品順序，採用瀑布流來顯示

--- src/main.js
+++ src/main.js
@@ -13,3 +13,12 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+// router - 1 作品集
+// pic - 1 魚克松
+// pic - 2 商品報修系統-app
+// pic - 3 德信投顧
+// pic - 4 北科大電子譜系統
+// pic - 5 樂子旅遊官方網站
+// pic - 6 issu-tracking
+// pic - 7 invisScreen
